6f320d9567fadfbb7554a27f297f40cf6712242c
yuefei-su <229681180@qq.com>
2022-09-23 16:32:04 +0800

    fill和获取key-value方法

---
+++ js/test.js
@@ -0,0 +1,17 @@
+for (let index of ['a', 'b'].keys()) {
+    console.log(index);
+}
+// 0
+// 1
+
+for (let elem of ['a', 'b'].values()) {
+    console.log(elem);
+}
+// 'a'
+// 'b'
+
+for (let [index, elem] of ['a', 'b'].entries()) {
+    console.log(index, elem);
+}
+// 0 "a"
+// 1 'b'
